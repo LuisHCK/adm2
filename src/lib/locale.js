a76d2fd7f3196214bca28600a2563da2c26ecef3
Luis J. Centeno <luisjcenteno@live.com>
2020-06-21 17:34:11 -0600

    Added spanish locales for time

---
+++ src/lib/locale.js
@@ -0,0 +1,26 @@
+export const months = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre'
+]
+
+export const daysAbr = ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom']
+
+export const days = [
+  'Lunes',
+  'Martes',
+  'Miércoles',
+  'Jueves',
+  'Viernes',
+  'Sábado',
+  'Domingo'
+]
