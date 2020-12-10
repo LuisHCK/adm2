2c844e3b46acb33af2a3b8e233b97267b454fa2b
Luis J. Centeno <luisjcenteno@live.com>
2020-12-10 16:05:51 -0600

    Reload page on backgup complete

--- src/db/backup.js
+++ src/db/backup.js
@@ -7,47 +7,49 @@ let progress = 0
 
 // Database export
 function backupDatabase(database) {
-  // Notify progress
-  EventBus.$emit('EXPORTING_DB', progress)
+    // Notify progress
+    EventBus.$emit('EXPORTING_DB', progress)
 
-  return exportDB(database, { progressCallback })
-    .then(blob => {
-      FileSaver.saveAs(blob, `${getFilename()}.json`)
-    })
-    .catch(err => {
-      console.error(err)
-    })
-    .finally(() => {
-      EventBus.$emit('IMPORT_FINISH')
-    })
+    return exportDB(database, { progressCallback })
+        .then(blob => {
+            FileSaver.saveAs(blob, `${getFilename()}.json`)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+        .finally(() => {
+            EventBus.$emit('IMPORT_FINISH')
+        })
 }
 
 function restoreDatabase(database, blob) {
-  EventBus.$buefy.dialog.confirm({
-    title: '¡CUIDADO!',
-    message: `Está a punto de cargar una copia de seguridad, esta opción
+    EventBus.$buefy.dialog.confirm({
+        title: '¡CUIDADO!',
+        message: `Está a punto de cargar una copia de seguridad, esta opción
       <b>sobreescribirá cualquier información</b> que haya creado anteriormente.<br/>
       ¿Deseas continuar?`,
-    confirmText: 'Si, contunar',
-    cancelText: 'Cancelar',
-    type: 'is-danger',
-    hasIcon: true,
-    onConfirm: () => {
-      EventBus.$emit('IMPORTING_DB', progress)
-      importInto(database, blob, {
-        progressCallback,
-        overwriteValues: true,
-        clearTablesBeforeImport: true
-      })
-        .then(db => {
-          console.log(db)
-        })
-        .catch(err => {
-          console.error(err)
-        })
-        .finally(EventBus.$emit('IMPORT_FINISH'))
-    }
-  })
+        confirmText: 'Si, contunar',
+        cancelText: 'Cancelar',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: () => {
+            EventBus.$emit('IMPORTING_DB', progress)
+            importInto(database, blob, {
+                progressCallback,
+                overwriteValues: true,
+                clearTablesBeforeImport: true
+            })
+                .then(db => {
+                    setTimeout(() => {
+                        location.reload(true)
+                    }, 500)
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+                .finally(EventBus.$emit('IMPORT_FINISH'))
+        }
+    })
 }
 
 /**
@@ -55,11 +57,11 @@ function restoreDatabase(database, blob) {
  * @param {Object} options
  */
 function progressCallback({ totalRows, completedRows }) {
-  progress = (100 * completedRows) / totalRows
+    progress = (100 * completedRows) / totalRows
 }
 
 function getFilename() {
-  return moment().format('lll')
+    return moment().format('lll')
 }
 
 export { backupDatabase, restoreDatabase }
