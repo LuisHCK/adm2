2d91d6b2bb6667dbe2e75b054755d6c6f4d2dc03
Luis J. Centeno <luisjcenteno@live.com>
2020-12-18 16:30:23 -0600

    Create Pos.vue

---
+++ src/components/settings/Pos.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: "pos",
+    
+    data() {
+        return {
+            
+        }
+    },
+
+    methods: {
+    
+    }
+}
+</script>
